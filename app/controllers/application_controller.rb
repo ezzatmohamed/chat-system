3c602d387e9b6e1e4e82e50341090e0c8871d616
ezzatmohamed <ezzat.mohamed2021@gmail.com>
2022-02-11 19:05:23 +0200

    add constants for pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,16 @@
 class ApplicationController < ActionController::API
+    MAX_LIMIT = 100
+    DEFAULT_LIMIT = 10
+    DEFAULT_OFFSET = 0
+
     def limit
         [
-            params.fetch(:limit, 10).to_i,
-            100
+            params.fetch(:limit, DEFAULT_LIMIT).to_i,
+            MAX_LIMIT
         ].min
     end
 
     def offset
-        params.fetch(:offset, 0)
+        params.fetch(:offset, DEFAULT_OFFSET)
     end
 end
